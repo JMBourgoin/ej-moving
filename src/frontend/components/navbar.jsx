fcd3891f53c185bb47e39d2fd13ede3a5d8a1061
Jason Bourgoin <jason@jmbourgoin.com>
2019-06-22 19:04:35 -0400

    update all

--- src/frontend/components/navbar.jsx
+++ src/frontend/components/navbar.jsx
@@ -10,7 +10,7 @@ class NavBarContainer extends React.Component {
         return(
             <div className='nav-container container-fluid'>
                 <div className='d-flex flex-row justify-content-between align-items-center'>
-                    <h2>contact: <a href="mailto:jason@jmbourgoin.com?subject=Moving%20Sale%20Inquiry">jason@jmbourgoin.com</a></h2>
+                    <h4>contact: <a href="mailto:jason@jmbourgoin.com?subject=Moving%20Sale%20Inquiry">jason@jmbourgoin.com</a></h4>
                     <h1 className='nav-head'>E & J's MOVING SALE</h1>
                     <ul className='nav-list d-flex flex-row-reverse'>
                         <li><a href="./index.html#/furniture">furniture</a></li>
